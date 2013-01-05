c3dffe65a2f54ec10d135809d99c1dc79361e5ff
vanyog <info@vanyog.com>
2013-01-05 16:15:02 +0200

    new file bg_spell/tables.sql

---
+++ bg_spell/tables.sql
@@ -0,0 +1,11 @@
+INSERT INTO `content` (`name`,`date_time_1`,`date_time_2`,`language`,`text`) VALUES
+('bg_spell_1',NOW(),NOW(),'bg','Напишете дума и щракнете бутона, за да видите дали я пишете правилно.'),
+('bg_spell_b',NOW(),NOW(),'bg','Проверка'),
+('bg_spell_2',NOW(),NOW(),'bg','<p>Проверяват се <strong>само отделни думи на кирилица</strong>, написани с малки букви! (Само личните съществителни  имена започват с главна буква.) За съставни думи с тире проверете отделните им части.</p>\r\n<hr>\r\n<p>Ако се питате дали две думи е правилно да се пишат слято или разделено, вижте <a href="http://bg.wikipedia.org/wiki/%D0%A1%D0%BB%D1%8F%D1%82%D0%BE%2C_%D0%BF%D0%BE%D0%BB%D1%83%D1%81%D0%BB%D1%8F%D1%82%D0%BE_%D0%B8_%D1%80%D0%B0%D0%B7%D0%B4%D0%B5%D0%BB%D0%BD%D0%BE_%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B5_%D0%B2_%D0%B1%D1%8A%D0%BB%D0%B3%D0%B0%D1%80%D1%81%D0%BA%D0%B8%D1%8F_%D0%B5%D0%B7%D0%B8%D0%BA" target="_blank">bg.wikipedia.org</A>.</p>\r\n<p><a href="http://vanyog.com/_new/index.php?pid=9" target="_blank">Още относно този речник</a>.\r\n<hr>'),
+('bg_spell_dl',NOW(),NOW(),'bg','База данни: <a href="http://physics-bg.org/z/index.php?pid=2" target="_blank">Речник на българския език</a>'),
+('bg_spell_word',NOW(),NOW(),'bg','Думата'),
+('bg_spell_ok',NOW(),NOW(),'bg','е написана правилно'),
+('bg_spell_nok',NOW(),NOW(),'bg','НЕ Е написана правилно или липсва в речника.'),
+('bg_spell_more',NOW(),NOW(),'bg','Още за тази дума'),
+('bg_spell_pr',NOW(),NOW(),'bg','Вижте предложенията за'),
+('bg_spell_cr',NOW(),NOW(),'bg','най-вероятна правилна дума');
